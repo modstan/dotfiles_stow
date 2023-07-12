3071d2747f23a1359cbd3313f74432b3ccbc6750
Modestas Stankus <stankus.modestas@gmail.com>
2023-07-12 07:40:28 +0300

    hyprland: keybindings, window rules, pyprland, split configs

--- hypr/.config/hypr/bin/socker_listener.sh
+++ hypr/.config/hypr/bin/socker_listener.sh
@@ -3,17 +3,7 @@
 handle() {
   echo "|$@|" >> /tmp/soclistener.log
   case $1 in monitoradded*)
-    hyprctl dispatch moveworkspacetomonitor "1 1"
-    hyprctl dispatch moveworkspacetomonitor "2 1"
-    hyprctl dispatch moveworkspacetomonitor "3 1"
-    hyprctl dispatch moveworkspacetomonitor "4 1"
-    hyprctl dispatch moveworkspacetomonitor "5 1"
-    hyprctl dispatch moveworkspacetomonitor "6 0"
-    hyprctl dispatch moveworkspacetomonitor "7 0"
-    hyprctl dispatch moveworkspacetomonitor "8 0"
-    hyprctl dispatch moveworkspacetomonitor "9 0"
-    hyprctl dispatch moveworkspacetomonitor "10 0"
-
+    ~/.config/hypr/bin/move-workspace.sh
   esac
 }
 
